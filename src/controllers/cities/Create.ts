c437e7fa506a9e1e1ed8061c57887b4d2dccb775
jadson santos <jadsoonbol@gmail.com>
2024-04-23 00:22:57 -0300

    yup multi-validation

--- src/controllers/cities/Create.ts
+++ src/controllers/cities/Create.ts
@@ -3,31 +3,39 @@ import { StatusCodes } from "http-status-codes";
 import * as yup from 'yup';
 
 interface ICity {
-    nome: String
+    nome: String,
+    estado: String
 }
 
 const bodyValidation: yup.ObjectSchema<ICity> = yup.object({
-  nome: yup.string().required().min(3) 
+    nome: yup.string().required().min(3),
+    estado: yup.string().required().min(2)
 })
 
 export const create = async (req: Request<{}, {}, ICity>, res: Response) => {
     let validateData: ICity | undefined = undefined;
 
-    try{
-        validateData = await bodyValidation.validate(req.body);
+    try {
+        validateData = await bodyValidation.validate(req.body, { abortEarly: false });
     } catch (e) {
         const yupError = e as yup.ValidationError;
+        const validationErrors: Record<string, string> = {};
 
-        return res.status(StatusCodes.BAD_GATEWAY).json({
-            errors: {
-                default: yupError.message,
-            }
+        yupError.inner.forEach(e => {
+            if(!e.path) return;
+
+            validationErrors[e.path] = e.message;
+
+        });
+
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            errors: validationErrors
         });
     }
-    
+
 
     console.log(validateData);
-    return res.status(StatusCodes.ACCEPTED).json({'nome' : 'Tobias Barreto'});
+    return res.status(StatusCodes.ACCEPTED).json({ 'nome': 'Tobias Barreto' });
 
 
 }
